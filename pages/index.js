125dbaa05137dde0abcbf9e3df9b57ec8c09ae42
piyushmittal20 <20piyushmittal@gmail.com>
2023-03-05 00:46:50 +0530

    code pushed

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,35 @@
 import axios from "axios";
 import Head from "next/head";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Add from "../components/Add";
 import AddButton from "../components/AddButton";
 import Featured from "../components/Featured";
 import PizzaList from "../components/PizzaList";
 import styles from "../styles/Home.module.css";
 
-export default function Home({ pizzaList, admin }) {
+export default function Home() {
   const [close, setClose] = useState(true);
+  const [pizzaList, setPizzaList] = useState([])
+
+  const fetchPizzaList = async () => {
+    try {
+      const { data } = await axios.get("/api/products");
+      return data
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    const getData = async () => {
+      const res = await fetchPizzaList()
+      setPizzaList(res)
+    }
+
+    getData()
+  }, [])
+
   return (
     <div className={styles.container}>
       <Head>
@@ -25,19 +45,19 @@ export default function Home({ pizzaList, admin }) {
   );
 }
 
-export const getServerSideProps = async (ctx) => {
-  const myCookie = ctx.req?.cookies || "";
-  let admin = false;
+// export const getServerSideProps = async (ctx) => {
+//   const myCookie = ctx.req?.cookies || "";
+//   let admin = false;
 
-  if (myCookie.token === process.env.TOKEN) {
-    admin = true;
-  }
+//   if (myCookie.token === process.env.TOKEN) {
+//     admin = true;
+//   }
 
-  const res = await axios.get("http://localhost:3000/api/products");
-  return {
-    props: {
-      pizzaList: res.data,
-      admin,
-    },
-  };
-};
+//   const { data } = await axios.get("http://localhost:3000/api/products");
+//   return {
+//     props: {
+//       pizzaList: data,
+//       admin,
+//     },
+//   };
+// };
